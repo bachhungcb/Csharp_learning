821cef6ea55c98e7d89cc43f3e794d1fa7bc20b9
bachhungcb <bachhungcb@gmail.com>
2024-03-14 12:23:34 +0700

    string in C#

---
+++ 2. Strings/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string firstFriend = "Maria";
+string secondFriend = "Huy";
+Console.WriteLine($"My first friend is {firstFriend} and my second friend is {secondFriend}");
