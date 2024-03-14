55465eddfc8e287951cdb9af8fca92dd70ec3cdc
bachhungcb <bachhungcb@gmail.com>
2024-03-14 12:26:55 +0700

    learn more about string in C#

--- 2. Strings/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-string firstFriend = "Maria";
-string secondFriend = "Huy";
-Console.WriteLine($"My first friend is {firstFriend} and my second friend is {secondFriend}");
