6f018e66af50772ab23e99cea5771672c774f922
bachhungcb <bachhungcb@gmail.com>
2024-03-26 22:33:56 +0700

    learn what is methods in C#

--- 21.Array/21.Array/obj/Release/net8.0/21.Array.AssemblyInfo.cs
+++ 21.Array/21.Array/obj/Release/net8.0/21.Array.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("21.Array")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ecf46f73d64cb54b1979ceabcc024bb560ad062")]
 [assembly: System.Reflection.AssemblyProductAttribute("21.Array")]
---
+++ 23.Methods/23.Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _23.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //methods = performs a section of code, whenever it is called 'invoked'.
+            //          benefit = Let's us reuse code w/o writing the same code again.
+            string name = "bach";
+            int age = 20;
+
+            singHappyBirthday(name, age);
+            singHappyBirthday(name, age);
+            singHappyBirthday(name, age); 
+
+        }
+
+        static void singHappyBirthday(string name, int age)
+        {
+            Console.WriteLine("Happy birthday to you");
+            Console.WriteLine("Happy birthday to you");
+            Console.WriteLine("Happy birthday dear " + name);
+            Console.WriteLine("Happy birthday to you");
+            Console.WriteLine("You are " + age + " years old");
+            Console.WriteLine();
+        }
+    }
+}
