170b2c73347b59313d33ec4fced5869a3f2eab2b
bachhungcb <bachhungcb@gmail.com>
2024-03-26 10:04:56 +0700

    learn about data type convertion

---
+++ 5.TypeCasting/5.TypeCasting/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//type casting  = Converting a value to a different data type 
+//                Usefull when you want to convert a value to a different data type
+
+double a = 3.14;
+int b = Convert.ToInt32(a);
+int c = 1243;
+double d = Convert.ToDouble(c) + 0.1;
+int i = 21321;
+string f = Convert.ToString(i);
+
+string g = "f";
+char h = Convert.ToChar(g);
+
+string j = "true";
+bool k = Convert.ToBoolean(j);
+
+Console.WriteLine(k);
+
+Console.WriteLine(f.GetType());
+Console.WriteLine(f);
+
