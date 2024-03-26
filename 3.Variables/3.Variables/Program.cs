d812436cfe84c128bcd3afe3ec77d5be44baef9f
bachhungcb <bachhungcb@gmail.com>
2024-03-26 09:53:31 +0700

    learn about variables in C#

---
+++ 3.Variables/3.Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x; // Declare a variable
+x = 10; //Initialize the variable
+
+int y = 123;
+
+int z = x + y;
+
+int age = 10;
+double height = 1.233;
+bool isStudent = true;
+
+char symbol = '!';
+string name = "Bach";
+
+Console.WriteLine("Your age is " + age + " years old");
+Console.WriteLine("Your height is " + height + " m");
+Console.WriteLine(isStudent);
+Console.WriteLine("Your symbol is " + symbol);
+Console.WriteLine("Your name is " + name);
+
+string username = symbol + name;
+
+Console.WriteLine("Your username is " + username);
+
+Console.ReadKey();
