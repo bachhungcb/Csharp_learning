68d1d2f1fd232cf45d9c621cc5e69f516c5889f4
bachhungcb <bachhungcb@gmail.com>
2024-03-26 13:51:39 +0700

    learn how to take user input in C#

--- 5.TypeCasting/5.TypeCasting/obj/Release/net8.0/5.TypeCasting.AssemblyInfo.cs
+++ 5.TypeCasting/5.TypeCasting/obj/Release/net8.0/5.TypeCasting.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("5.TypeCasting")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+170b2c73347b59313d33ec4fced5869a3f2eab2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("5.TypeCasting")]
---
+++ 6.UserInput/6.UserInput/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("What is your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+Console.WriteLine("Hello " + name);
+Console.WriteLine("Your are " + age + " years old");
