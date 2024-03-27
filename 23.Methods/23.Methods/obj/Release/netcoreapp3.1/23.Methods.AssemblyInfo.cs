8fac63bb167c93f13ae77508d9cfee0fd7eb4995
bachhungcb <bachhungcb@gmail.com>
2024-03-27 15:55:27 +0700

    .

--- 23.Methods/23.Methods/obj/Release/netcoreapp3.1/23.Methods.AssemblyInfo.cs
+++ 23.Methods/23.Methods/obj/Release/netcoreapp3.1/23.Methods.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("23.Methods")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f018e66af50772ab23e99cea5771672c774f922")]
 [assembly: System.Reflection.AssemblyProductAttribute("23.Methods")]
