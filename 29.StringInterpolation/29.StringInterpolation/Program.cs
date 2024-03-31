8cfe6b0c3bc81737b6dd514991dea1a39acd1daf
bachhungcb <bachhungcb@gmail.com>
2024-03-31 21:43:45 +0700

    Learn about string interpolation in C#

---
+++ 29.StringInterpolation/29.StringInterpolation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _29.StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string interpolation = allows us to insert variables into a string literal
+            //                       precede a string literal with $
+            //                       {} are placeholders
+
+            String firstName = "bach";
+            string lastName = "Dam";
+            int age = 21;
+
+            Console.WriteLine($"Hello {firstName} {lastName}.");
+            Console.WriteLine($"Your are {age} years old.");
+
+            Console.ReadKey();
+        }
+    }
+}
