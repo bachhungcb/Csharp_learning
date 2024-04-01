bbaa20b091a1b477440e0d7847a7192231af9b35
bachhungcb <bachhungcb@gmail.com>
2024-04-01 13:33:10 +0700

    learn about classes in C#

---
+++ 31.Classes/31.Classes/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _31.Classes
+{
+    static class Message
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I'm waiting for you");
+        }
+
+        public static void Bye()
+        {
+            Console.WriteLine("Bye, see you again");
+        }
+    }
+
+}
---
+++ 31.Classes/31.Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _31.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //class = A bundle of related code
+            //        Can be used as a blueprint to create object (OOP)
+
+            Message.Hello();
+            Message.Waiting();
+            Message.Bye();
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
